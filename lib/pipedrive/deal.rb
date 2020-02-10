59e0ee0a7ed4c8057e9c7b377aa48f428b58300c
Marian Theisen <marian@cice-online.net>
2020-02-10 09:24:37 +0100

    wip

--- lib/pipedrive/deal.rb
+++ lib/pipedrive/deal.rb
@@ -4,5 +4,9 @@ module Pipedrive
     include ::Pipedrive::Operations::Read
     include ::Pipedrive::Operations::Update
     include ::Pipedrive::Operations::Delete
+
+    def add_product(deal_id, params)
+      make_api_call(:post, "#{deal_id}/products", params)
+    end
   end
 end
